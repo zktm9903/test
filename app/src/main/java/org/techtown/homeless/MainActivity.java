34f086fdb0e83eea5cace1ba49053e8f673a571c
zktm9903 <57246460+zktm9903@users.noreply.github.com>
2021-02-10 21:41:29 +0900

    test3

--- app/src/main/java/org/techtown/homeless/MainActivity.java
+++ app/src/main/java/org/techtown/homeless/MainActivity.java
@@ -3,17 +3,37 @@ package org.techtown.homeless;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView won;
+    LinearLayout layout1;
+    int money = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
         setContentView(R.layout.activity_main);
+
+        won = (TextView) findViewById(R.id.won);
+        layout1 = (LinearLayout) findViewById(R.id.layout1);
+
+        layout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                money++;
+                won.setText(money + " WON");
+            }
+        });
     }
 }
